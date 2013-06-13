b49e82dac5355ec87a6bfce21e99a564df0993e2
Jimmy <jkao@live.ca>
2013-06-13 13:20:23 -0700

    New system
    re-done everything, home page is done, doing product page

---
+++ product/toyfactory.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$(".change1").click(function() {
+		$("#large").css("background-image", "url('http://www.3dwallz.com/wp-content/uploads/2012/11/NatureWallpapers-50.jpg')");
+	});
+});
+
+
+$(document).ready(function() {
+	$(".change2").click(function() {
+		$("#large").css("background-image", "url('http://www.hdwallpapers3d.com/wp-content/uploads/2013/04/hd-wallpapers-1080p1.jpg')");
+	});
+});
+
+$(document).ready(function() {
+	$(".change3").click(function() {
+		$("#large").css("background-image", "url('http://2.bp.blogspot.com/_kw1VD16J_Xw/SnLHlarE4sI/AAAAAAAACZc/rUxZIX9-o20/s1600/HDTV_monaco_1920x1080.jpg')");
+	});
+});
+
+$(document).ready(function() {
+	$(".change4").click(function() {
+		$("#large").css("background-image", "url('http://www.hdwallpaperspk.com/wp-content/uploads/2013/02/nature-hd-theme-bin-customization-hd.jpg')");
+	});
+});s
