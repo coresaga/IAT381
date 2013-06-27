732b475a5e5845ef6a8c16404b6504a3f46eecab
Jimmy <jkao@live.ca>
2013-06-27 14:29:17 -0700

    tweaks and fixes

--- product/feedback/toyfactory.js
+++ product/feedback/toyfactory.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
 	$(".change1").click(function() {
-		$("#large").css("background-image", "url('http://www.3dwallz.com/wp-content/uploads/2012/11/NatureWallpapers-50.jpg')");
+		$("#large").css("background-image", "url('../../img/1.jpg')");
 	});
 });
 
 
 $(document).ready(function() {
 	$(".change2").click(function() {
-		$("#large").css("background-image", "url('http://www.hdwallpapers3d.com/wp-content/uploads/2013/04/hd-wallpapers-1080p1.jpg')");
+		$("#large").css("background-image", "url('../../img/2.jpg')");
 	});
 });
 
 $(document).ready(function() {
 	$(".change3").click(function() {
-		$("#large").css("background-image", "url('http://2.bp.blogspot.com/_kw1VD16J_Xw/SnLHlarE4sI/AAAAAAAACZc/rUxZIX9-o20/s1600/HDTV_monaco_1920x1080.jpg')");
+		$("#large").css("background-image", "url('../../img/3.jpg')");
 	});
 });
 
 $(document).ready(function() {
 	$(".change4").click(function() {
-		$("#large").css("background-image", "url('http://www.hdwallpaperspk.com/wp-content/uploads/2013/02/nature-hd-theme-bin-customization-hd.jpg')");
+		$("#large").css("background-image", "url('../../img/4.jpg')");
 	});
 });s
