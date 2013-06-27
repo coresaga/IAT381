cb32723c5d342d64a110543cdca2d958c5b015ae
Jimmy <jkao@live.ca>
2013-06-27 02:24:26 -0700

    pictures linked and added

--- toyfactory.js
+++ toyfactory.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
 	$(".change1").click(function() {
-		$("#canvas").css("background-image", "url('http://www.3dwallz.com/wp-content/uploads/2012/11/NatureWallpapers-50.jpg')");
+		$("#canvas").css("background-image", "url('./img/Munny.jpg')");
 	});
 });
 
 
 $(document).ready(function() {
 	$(".change2").click(function() {
-		$("#canvas").css("background-image", "url('http://www.hdwallpapers3d.com/wp-content/uploads/2013/04/hd-wallpapers-1080p1.jpg')");
+		$("#canvas").css("background-image", "url('./img/legolotr.jpg')");
 	});
 });
 
 $(document).ready(function() {
 	$(".change3").click(function() {
-		$("#canvas").css("background-image", "url('http://2.bp.blogspot.com/_kw1VD16J_Xw/SnLHlarE4sI/AAAAAAAACZc/rUxZIX9-o20/s1600/HDTV_monaco_1920x1080.jpg')");
+		$("#canvas").css("background-image", "url('./img/supersoaker.jpg')");
 	});
 });
 
 $(document).ready(function() {
 	$(".change4").click(function() {
-		$("#canvas").css("background-image", "url('http://www.hdwallpaperspk.com/wp-content/uploads/2013/02/nature-hd-theme-bin-customization-hd.jpg')");
+		$("#canvas").css("background-image", "url('./img/disney.jpg')");
 	});
 });
